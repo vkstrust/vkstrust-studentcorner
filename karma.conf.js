a6c1b749300e51e359d455b441e8c0950d2567f9
ajithK24 <ajithkumar@jobuli.in>
2022-06-06 06:48:13 +0530

    changed name angular heros to vks

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-tour-of-heroes'),
+      dir: require('path').join(__dirname, './coverage/VKS TRUST'),
       subdir: '.',
       reporters: [
         { type: 'html' },
